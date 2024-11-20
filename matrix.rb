c7ddec436607ce71a2f7fe50e6bc4e1e53c1ec56
Shraddha <166834534+shrads08@users.noreply.github.com>
2024-11-20 06:41:21 +0000

    notes

--- matrix.rb
+++ matrix.rb
@@ -16,5 +16,7 @@ end
 
 =begin
 ---------------------------Learnings-------------------------------------
-
+1. At each "\n" split operation is performed and then 1st map operation which gives a row of string values that are ultimately converted to integers via .map(&:to_i) 
+2. map(&:to_i) is a shorthand way to convert each element of an array to an integer using the to_i method.
+3. transpose method is used to transpose the matrix i.e. convert rows to columns and vice-versa.
 =end
